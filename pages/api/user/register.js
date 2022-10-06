40299efcf4f9e19f2cfd5adf5fdf98281e3ac663
Chonlanan Thongthai <chonlanan_t@cmu.ac.th>
2022-10-06 15:10:12 +0700

    Quiz04 finished

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -5,9 +5,15 @@ import { checkToken } from "../../../backendLibs/checkToken";
 export default function userRegisterRoute(req, res) {
   if (req.method === "POST") {
     const { username, password, isAdmin } = req.body;
+    const user = checkToken(req);
 
     //check authentication
-    const user = checkToken(req);
+    if (!user || !user.isAdmin)
+      return res.status(403).json({
+        ok: false,
+        message: "You do not have permission to create account",
+      });
+
     //return res.status(403).json({ok: false,message: "You do not have permission to create account",});
 
     //validate body
@@ -24,12 +30,23 @@ export default function userRegisterRoute(req, res) {
 
     //check if username is already in database
     const users = readUsersDB();
-    //return res.status(400).json({ ok: false, message: "Username is already taken" });
+    const foundUser = users.find((x) => x.username === username);
+    if (foundUser)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username is already taken" });
 
-    //create new user and add in db
+    const newUser = {
+      username,
+      password: bcrypt.hashSync(password, 12),
+      isAdmin,
+      money: isAdmin ? null : 0,
+    };
 
+    users.push(newUser);
     writeUsersDB(users);
 
-    //return response
+    //send username back when successfully registered
+    return res.json({ ok: true, username, isAdmin });
   }
 }
